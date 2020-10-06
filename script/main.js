ff048752d6aaacae873dababb2a3c69c56ae8506
brilook <brilook@gmail.com>
2020-10-06 15:41:59 +0300

    generate all items page

--- script/main.js
+++ script/main.js
@@ -1,9 +1,15 @@
+'use strict';
+
 const btnBurger = document.querySelector('.btn-burger');
 const catalog = document.querySelector('.catalog');
-const overlay = document.querySelector('.overlay');
 const btnClose = document.querySelector('.btn-close');
-const subcatalog = document.querySelector('.subcatalog')
-const subcatalogHeader = document.querySelector('.subcatalog-header')
+const subcatalog = document.querySelector('.subcatalog');
+const subcatalogHeader = document.querySelector('.subcatalog-header');
+const btnReturn = document.querySelector('.btn-return');
+
+const overlay = document.createElement('div');
+overlay.classList.add('overlay');
+document.body.insertAdjacentElement('beforeend', overlay);
 
 const openMenu = () => {
   catalog.classList.add('open');
@@ -13,6 +19,7 @@ const openMenu = () => {
 const closeMenu = () => {
   catalog.classList.remove('open');
   overlay.classList.remove('active');
+  closeSubMenu();
 };
 
 const openSubMenu = event => {
@@ -23,6 +30,11 @@ const openSubMenu = event => {
     subcatalogHeader.innerHTML = itemList.innerHTML;
     subcatalog.classList.add('subopen')
   };
+};
+
+const closeSubMenu = () => {
+  subcatalog.classList.remove('subopen')
+
 }
 
 btnBurger.addEventListener('click', openMenu);
@@ -32,3 +44,4 @@ catalog.addEventListener('click', openSubMenu);
 document.addEventListener('keydown', event => {
   if (event.code === 'Escape') closeMenu();
 });
+btnReturn.addEventListener('click', closeSubMenu);
